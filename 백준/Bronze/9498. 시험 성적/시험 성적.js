a56cd2bdbc82af477eaf9cebe1f02f3466817017
황창현 <changhyeon.h@kakao.com>
2023-02-08 23:08:21 +0900

    [Bronze V] Title: 시험 성적, Time: 132 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const input = parseInt(fs.readFileSync('/dev/stdin').toString().trim());
+
+if(input <= 100 && input >= 90) console.log('A');
+else if(input <= 89 && input >= 80) console.log('B');
+else if(input <= 79 && input >= 70) console.log('C');
+else if(input <= 69 && input >= 60) console.log('D');
+else console.log('F');
