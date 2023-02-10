214162448c06873b5b4c7bee9cf5d2e32cfa4f90
황창현 <changhyeon.h@kakao.com>
2023-02-10 21:14:26 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.73 ms, Memory: 30.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -0,0 +1,44 @@
+function solution(new_id) {
+    let answer = '';
+    let arr = [];
+    let pattern_eng = /[a-z]/;
+    let pattern_num = /[0-9]/;
+    answer = new_id.toLowerCase();
+    arr = answer.split('');
+
+    let move = arr.filter(v => {
+        return pattern_eng.test(v) === true || pattern_num.test(v) === true || v == '-' || v=='_' || v=='.'
+    });
+
+    for(let i = 0; i < move.length; i++){
+        if(move[i] == '.' && move[i+1] == '.'){
+            move.splice(i,1);
+            i = -1;
+        }
+    }
+   
+    if(move[0] == '.'){
+        move.splice(0,1);
+    }
+    if(move[move.length-1] == '.'){
+        move.splice(move.length-1,1);
+    }
+    
+    if(move.length == 0){
+        move.push('a');
+    }
+    
+    if(move.length > 15){
+        move.splice(15, move.length-15);
+        if(move[move.length-1] == '.'){
+            move.splice(move.length-1,1);
+        }
+    }
+    if(move.length < 3){
+        while(move.length < 3) {
+            move.push(move[move.length-1]);
+        }  
+    }
+    answer = move.join('');
+    return answer;
+}
