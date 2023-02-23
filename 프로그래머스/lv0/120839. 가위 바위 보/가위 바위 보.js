43f9145c4c6597517a19bcd1c5a59e87bcfc3355
황창현 <changhyeon.h@kakao.com>
2023-02-23 19:57:18 +0900

    [level 0] Title: 가위 바위 보, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -0,0 +1,14 @@
+function solution(rsp) {
+    const rspArr = rsp.split('');
+    const resultArr = rspArr.map(item => {
+        if(item === '2'){
+            return '0';
+        } else if(item === '0'){
+            return '5';
+        } else {
+            return '2';
+        }
+    })
+    
+    return resultArr.join('');
+}
