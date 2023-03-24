fef7c7b8025d91b7f4c817c9bc3cad0f40e5cdf5
황창현 <changhyeon.h@kakao.com>
2023-03-24 13:57:02 +0900

    [level 2] Title: 숫자의 표현, Time: 1.65 ms, Memory: 36.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,18 @@
+function solution(n) {
+    let answer = 0;
+    for(let i=1; i < n; i++){
+        let sum = 0;
+        for(let j=i; j < n; j++){
+            sum += j;
+            if(sum === n) {
+                answer++; 
+                break;
+            }
+            if(sum+j+1 > n){
+                break;
+            }
+        }
+    }
+    
+    return answer+1;
+}
