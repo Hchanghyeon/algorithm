bfa90adee67cb62efe3387c581fb35753765a8df
황창현 <changhyeon.h@kakao.com>
2023-08-26 15:10:25 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,64 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
-    private final static Scanner scanner = new Scanner(System.in);
-    private final static List<List<Integer>> lines = new ArrayList<>();
+    private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private static final List<List<Integer>> lines = new ArrayList<>();
+    private static boolean[] visited;
+    private static int count;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        int peakNum = scanner.nextInt();
-        int lineNum = scanner.nextInt();
-        boolean isVisited[] = new boolean[peakNum + 1];
+        int computer = Integer.parseInt(bufferedReader.readLine());
+        int lineCount = Integer.parseInt(bufferedReader.readLine());
+        visited = new boolean[computer+1];
 
-        for(int i = 0; i <= peakNum; i++){
+        for(int i = 0; i <= computer; i++){
             lines.add(new ArrayList<>());
         }
 
-        for(int i = 0; i < lineNum; i++){
-            int num1 = scanner.nextInt();
-            int num2 = scanner.nextInt();
+        for(int i = 0; i < lineCount; i++){
+            String[] line = bufferedReader.readLine().split(" ");
+            int num1 = Integer.parseInt(line[0]);
+            int num2 = Integer.parseInt(line[1]);
 
             lines.get(num1).add(num2);
             lines.get(num2).add(num1);
         }
 
-        int i = bfs(isVisited);
-
-        System.out.println(i);
+        bfs(1);
+        System.out.println(count-1);
     }
 
-
-    private static int bfs(boolean[] visited){
+    private static void bfs(int i) {
         Queue<Integer> queue = new LinkedList<>();
-
-        queue.add(1);
-        visited[1] = true;
+        queue.add(i);
+        visited[i] = true;
 
         while(!queue.isEmpty()){
-            int peak = queue.poll();
+            int item = queue.poll();
 
-            for(int i = 0; i < lines.get(peak).size(); i++){
-                int linkedPeak = lines.get(peak).get(i);
-
-                if(!visited[linkedPeak]){
-                    queue.add(lines.get(peak).get(i));
-                    visited[lines.get(peak).get(i)] = true;
+            for(int j = 0; j < lines.get(item).size(); j++){
+                int num = lines.get(item).get(j);
+                if(!visited[num]){
+                    queue.add(num);
+                    visited[num] = true;
                 }
             }
         }
 
-        int count = 0;
-        for(boolean result : visited){
+        for(boolean result: visited){
             if(result){
                 count++;
             }
         }
-
-        return count-1;
     }
 }
