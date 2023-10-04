a5bc9b989843a06c28b96cf415532dabf8c2cb6d
황창현 <changhyeon.h@kakao.com>
2023-10-04 21:39:46 +0900

    [Bronze V] Title: 아스키 코드, Time: 128 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        System.out.println((int)bufferedReader.readLine().charAt(0));
+    }
+}
