fe2e3285e7de198452f8a96054f7ed3fc0b2b1eb
황창현 <changhyeon.h@kakao.com>
2023-12-27 11:28:37 +0900

    [Silver III] Title: N과 M (3), Time: 552 ms, Memory: 157896 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private static final BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static int N, M;
+    private static int[] nums;
+
+    public static void main(String[] args) throws IOException {
+
+        String[] line = bufferedReader.readLine().split(" ");
+        N = Integer.parseInt(line[0]);
+        M = Integer.parseInt(line[1]);
+
+        nums = new int[M];
+
+        dfs(0);
+        bufferedWriter.close();
+    }
+
+    private static void dfs(final int count) throws IOException {
+        if (count == M) {
+            for (int num : nums) {
+                bufferedWriter.write(num + " ");
+            }
+
+            bufferedWriter.newLine();
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            nums[count] = i + 1;
+            dfs(count + 1);
+        }
+    }
+}
