8af737a6c93f60aae7b1ad4380ac1ddf0702e3db
황창현 <changhyeon.h@kakao.com>
2023-12-29 08:26:14 +0900

    [Gold V] Title: 암호 만들기, Time: 168 ms, Memory: 16300 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,62 +1,66 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
-    static int L, C;
-    static String vows = "aeiou";
-    static List<String> con = new ArrayList<>();
-    static List<String> vow = new ArrayList<>();
-    static List<String> temp = new ArrayList<>();
-    static List<String> result = new ArrayList<>();
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        L = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < C; i++) {
-            String ap = st.nextToken();
-            if(vows.contains(ap))
-                vow.add(ap);
+    private static int L, C;
+    private static String vows = "aeiou";
+    private static List<String> con = new ArrayList<>();
+    private static List<String> vow = new ArrayList<>();
+    private static List<String> temp = new ArrayList<>();
+    private static List<String> result = new ArrayList<>();
+    private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        String[] line = bufferedReader.readLine().split(" ");
+        L = Integer.parseInt(line[0]);
+        C = Integer.parseInt(line[1]);
+
+        String[] strArr = bufferedReader.readLine().split(" ");
+        for (int i = 0; i < C; i++) {
+            String alpabet = strArr[i];
+            if (vows.contains(alpabet))
+                vow.add(alpabet);
             else
-                con.add(ap);
+                con.add(alpabet);
         }
 
-        for(int i = 1; i <= L-2; i++) {
+        for (int i = 1; i <= L - 2; i++) {
             vowDfs(i, 0, 0);
         }
 
         Collections.sort(result);
-        for(int i = 0; i < result.size(); i++) {
+        for (int i = 0; i < result.size(); i++) {
             System.out.println(result.get(i));
         }
     }
 
     public static void vowDfs(int depth, int index, int cnt) {
-        if(cnt == depth) {
-            conDfs(L-depth, 0, 0);
+        if (cnt == depth) {
+            conDfs(L - depth, 0, 0);
             return;
         }
-        for(int i = index; i < vow.size(); i++) {
+        for (int i = index; i < vow.size(); i++) {
             temp.add(vow.get(i));
-            vowDfs(depth, i+1, cnt+1);
+            vowDfs(depth, i + 1, cnt + 1);
             temp.remove(vow.get(i));
         }
     }
 
     public static void conDfs(int depth, int index, int cnt) {
-        if(cnt == depth) {
+        if (cnt == depth) {
             Collections.sort(temp);
             String str = String.join("", temp);
             result.add(str);
             return;
         }
-        for(int i = index; i < con.size(); i++) {
+        for (int i = index; i < con.size(); i++) {
             temp.add(con.get(i));
-            conDfs(depth, i+1, cnt+1);
+            conDfs(depth, i + 1, cnt + 1);
             temp.remove(con.get(i));
         }
     }
