bc04a3e6698599e8ade864b6e1ab924564d16c8e
황창현 <changhyeon.h@kakao.com>
2024-01-22 10:46:16 +0900

    [Silver I] Title: 미로 탐색, Time: 208 ms, Memory: 16928 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,66 +1,67 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
-import java.util.stream.Collectors;
+import java.util.Deque;
 
 public class Main {
 
     private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-    private static int height = 0;
-    private static int width = 0;
+    private static int N, M;
     private static int[][] map;
-    private static int[][] visited;
-    private static final int[] mx = {0, 1, 0, -1};
-    private static final int[] my = {-1, 0, 1, 0};
+    private static boolean[][] visited;
+    private static int[] mx = {0, 0, -1, 1};
+    private static int[] my = {-1, 1, 0, 0};
 
     public static void main(String[] args) throws IOException {
+        String[] line = bufferedReader.readLine().split(" ");
 
-        StringTokenizer token = new StringTokenizer(bufferedReader.readLine());
+        N = Integer.parseInt(line[0]);
+        M = Integer.parseInt(line[1]);
 
-        height = Integer.parseInt(token.nextToken());
-        width = Integer.parseInt(token.nextToken());
+        map = new int[N][M];
+        visited = new boolean[N][M];
 
-        map = new int[height][width];
-        visited = new int[height][width];
-
-        for(int i = 0; i < height; i++){
-            String[] arr = bufferedReader.readLine().split("");
-            map[i] = Arrays.stream(arr).mapToInt(Integer::valueOf).toArray();
+        for (int i = 0; i < N; i++) {
+            String[] temp = bufferedReader.readLine().split("");
+            map[i] = Arrays.stream(temp)
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
         }
 
         bfs(0, 0);
-
-        System.out.println(visited[height-1][width-1]);
+        System.out.println(map[N - 1][M - 1]);
     }
 
-    private static void bfs(int w, int h) {
-        Queue<List<Integer>> queue = new LinkedList<>();
-        queue.add(new ArrayList<>(Arrays.asList(w, h)));
-        visited[h][w] = 1;
+    private static void bfs(final int i, final int j) {
+        Deque<int[]> deque = new ArrayDeque<>();
+        deque.add(new int[] {i, j});
 
-        while(!queue.isEmpty()){
-            List<Integer> point = queue.poll();
+        while (!deque.isEmpty()) {
+            int[] point = deque.poll();
+            int x = point[0];
+            int y = point[1];
 
-            for(int i = 0; i < 4; i++){
-                int dx = mx[i] + point.get(0);
-                int dy = my[i] + point.get(1);
+            visited[y][x] = true;
 
-                if(isBoundary(dx, dy)){
-                    queue.add(new ArrayList<>(Arrays.asList(dx, dy)));
-                    visited[dy][dx] = visited[point.get(1)][point.get(0)] + 1;
+            for (int k = 0; k < 4; k++) {
+                int dx = mx[k] + x;
+                int dy = my[k] + y;
+
+                if (isBoundary(dx, dy)) {
+                    deque.add(new int[] {dx, dy});
+                    map[dy][dx] = map[y][x] + 1;
                 }
             }
         }
     }
 
-    private static boolean isBoundary(int dx, int dy) {
-        return dx < width && dx >= 0 && dy < height && dy >= 0 && map[dy][dx] != 0 && visited[dy][dx] == 0;
-    }
+    private static boolean isBoundary(final int dx, final int dy) {
+        if (dx >= 0 && dx < M && dy >= 0 && dy < N && !visited[dy][dx] && map[dy][dx] == 1) {
+            return true;
+        }
 
+        return false;
+    }
 }
