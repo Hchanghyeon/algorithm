d6dc74715a8f0ebff0492de50895ff1fb23b92c5
황창현 <changhyeon.h@kakao.com>
2024-02-13 14:57:09 +0900

    [level 2] Title: 의상, Time: 0.06 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int sum = 1;
+        
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < clothes.length; i++){
+            String category = clothes[i][1];    
+            map.put(category, map.getOrDefault(category, 0) + 1);
+        }
+        
+        for(int num : map.values()){
+            sum *= (num + 1);  // 안 입었을 때의 경우 1 추가
+        }
+        
+        return sum - 1;
+    }
+}
