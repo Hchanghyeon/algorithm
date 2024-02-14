cc5004549693357d7d00776bbca330d3fccd9529
황창현 <changhyeon.h@kakao.com>
2024-02-14 15:47:43 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 40.86 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {        
+        Deque<Integer> deque = new ArrayDeque<>();
+        
+        for(int number : arr){
+            if(deque.isEmpty()){
+                deque.add(number);
+                continue;
+            }
+
+            if(deque.peekLast() != number){
+                deque.add(number);
+            }
+        }
+        
+        int[] answer = new int[deque.size()];
+        
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = deque.poll();
+        }
+
+        return answer;
+    }
+}
