a41e268a4b5220a6f2adcbea51e3946e2cf33121
황창현 <changhyeon.h@kakao.com>
2024-02-18 15:24:03 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.54 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        int sum = 0;
+        int bridgeSum = 0;
+        int count = 0;
+        
+        Deque<Integer> wait = new ArrayDeque<>();
+        Deque<Truck> during = new ArrayDeque<>();
+        
+        for(int truckWeight : truck_weights){
+            wait.add(truckWeight);
+        }
+        
+        while(true){
+            if(wait.isEmpty() && during.isEmpty()){
+                break;
+            }
+            
+            count++; 
+            Truck duringTruck = during.peek();
+            
+            if(duringTruck != null){
+                if(duringTruck.getIndex() == bridge_length){
+                    during.poll();
+                    sum -= duringTruck.getWeight();
+                }
+            
+                for(Truck truck : during){
+                    truck.updateIndex();
+                }
+            }
+        
+            Integer truckWeight = wait.peek();
+            if(truckWeight != null){
+                if(weight < (truckWeight + sum) || during.size() >= bridge_length){
+                    continue;
+                }
+            
+                sum += truckWeight;
+                truckWeight = wait.poll();
+                during.add(new Truck(1, truckWeight));
+            }
+        }
+        
+        return count;
+    }
+    
+    static class Truck {
+        private int index;
+        private int weight;
+        
+        public Truck(int index, int weight){
+            this.index = index;
+            this.weight = weight;
+        }
+        
+        public void updateIndex(){
+            index++;
+        }
+        
+        public int getIndex(){
+            return index;
+        }
+        
+        public int getWeight(){
+            return weight;
+        }
+    }
+}
