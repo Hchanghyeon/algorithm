425704426bb44a91d2e2c23234662ee10489457e
황창현 <changhyeon.h@kakao.com>
2024-02-19 10:25:59 +0900

    [level 2] Title: 더 맵게, Time: 1599.25 ms, Memory: 123 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        
+        for(int number : scoville){
+            priorityQueue.add(number);
+        }
+        
+        
+        while(true){ 
+            if(priorityQueue.size() < 2 && priorityQueue.peek() < K){
+                return -1;
+            }
+            
+            Integer num1 = priorityQueue.peek();
+            
+            if(num1 >= K){
+                break;
+            } 
+            
+            priorityQueue.poll();
+            int num2 = priorityQueue.poll();
+            int newNumber = num1 + (num2 * 2);
+            priorityQueue.add(newNumber);
+            answer++;
+        }
+        
+        return answer;
+    }
+}
