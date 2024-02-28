d2357e9cee495de7735eaa11db5f8231fe740845
황창현 <changhyeon.h@kakao.com>
2024-02-28 10:21:58 +0900

    [level 2] Title: 구명보트, Time: 19.98 ms, Memory: 54.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int count = 0;
+        Arrays.sort(people);
+        Deque<Integer> deque = new ArrayDeque<>();
+        
+        for(int i = people.length - 1; i >= 0; i--){
+            deque.add(people[i]);
+        }
+        
+        while(deque.size() >= 2){
+            int first = deque.peekFirst();
+            int last = deque.peekLast();
+            
+            if(first + last <= limit){
+                deque.pollFirst();
+                deque.pollLast();
+                count++;
+                continue;
+            }
+            
+            deque.poll();
+            count++;
+        }
+        
+        if(!deque.isEmpty()){
+            deque.poll();
+            count++;
+        }
+        
+        return count;
+    }
+}
