f996e4066ae24169a2a724facf10d43760dd6c9d
황창현 <changhyeon.h@kakao.com>
2024-03-04 10:27:06 +0900

    [level 3] Title: 네트워크, Time: 0.08 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int n, int[][] computers) {
+        int count = 0;
+        boolean[] visited = new boolean[n];
+        
+        for(int i = 0; i < n; i++){
+            if(!visited[i]){
+                count++;
+                dfs(i, computers, visited);
+            }
+        }
+        
+        return count;
+    }
+    
+    private void dfs(int index, int[][] computers, boolean[] visited){
+        visited[index] = true;
+        for(int i = 0; i < computers[index].length; i++){
+            if(computers[index][i] == 1 && !visited[i]){
+                dfs(i, computers, visited);
+            }   
+        }
+    }
+}
